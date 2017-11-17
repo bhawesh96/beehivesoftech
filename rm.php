643cde5fbd786bee1b1e85130340dbd61debc8bd
bhawesh96 <bhansalibhawesh@yahoo.com>
2017-11-17 19:35:54 +0530

    rm.php added

--- rm.php
+++ rm.php
@@ -5,11 +5,23 @@ $email_address = strip_tags(htmlspecialchars($_GET['email']));
 $phone = strip_tags(htmlspecialchars($_GET['phone']));
 $message = strip_tags(htmlspecialchars($_GET['message']));
    
-$to = 'bhansalibhawesh85@gmail.com'; 
+$to = 'team.robomanipal@gmail.com'; 
 $email_subject = "From  $name";
-$email_body = "New message\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "Hello Robomanipal!\n\nYou have received a new message via the contact form of your website robomanipal.com\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: webmail\n";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+echo "message successfully sent";
+echo "
+<script>
+window.location = 'http://robomanipal.com/contact.html';
+</script>
+";
+
+
+return true;
+
+
 ?>
+
